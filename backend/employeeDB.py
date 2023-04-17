6dfde969aa6a43190e7d1c38ca3bddf296aee101
GyDani <93887003+GyDani@users.noreply.github.com>
2023-04-17 13:30:23 +0200

    Update employeeDB.py

--- backend/employeeDB.py
+++ backend/employeeDB.py
@@ -4,7 +4,7 @@ con = sqlite3.connect("employee.db")
 print("Database opened successfully")
 
 con.execute(
-    "CREATE TABLE Cars (id INTEGER, marka TEXT NOT NULL, ev TEXT NOT NULL UNIQUE, km TEXT NOT NULL, hasznalt TEXT NOT NULL, muszaki	TEXT, rendszam	TEXT, motor TEXT, loero TEXT, gyorsulas TEXT, sebesseg TEXT, fordulat TEXT, suly TEXT, sebvaltoTipus TEXT, sebvaltoSzam TEXT, fogyasztas TEXT, benzin TEXT, biztonsag TEXT, atalakitas TEXT, kiegeszites TEXT, PRIMARY KEY(id AUTOINCREMENT)");
+    "CREATE TABLE Cars (id INTEGER, marka TEXT NOT NULL, ev TEXT NOT NULL UNIQUE, km TEXT NOT NULL, hasznalt TEXT NOT NULL, muszaki	TEXT, rendszam	TEXT, motor TEXT, loero TEXT, gyorsulas TEXT, sebesseg TEXT, fordulat TEXT, suly TEXT, sebvaltoTipus TEXT, sebvaltoSzam TEXT, fogyasztas TEXT, benzin TEXT, biztonsag TEXT, atalakitas TEXT, kiegeszites TEXT, PRIMARY KEY(id AUTOINCREMENT)")
 
 print("Table created successfully")
 
